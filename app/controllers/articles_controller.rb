c15e15cccc5acb99185649e572b8eb1d5f71eed0
nielsbischoff <niels.bischoff@gmail.com>
2015-06-17 17:24:44 -0700

    adds edit/update articles and flashes for delete edit and new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,15 +18,30 @@ class ArticlesController < ApplicationController
 	    @article.title = params[:article][:title]
 	    @article.save
 
+	    flash.notice = "Awesome! Article '#{@article.title}' has been created :)"
+
 	    redirect_to article_path(@article)
   	end
 
   	def destroy
   		@article = Article.find(params[:id])
   		@article.delete 
+
+  		flash.notice = "Article '#{@article.title}' has been deleted :("
+
   		redirect_to articles_path
   	end	
 
+  	def edit
+  		@article = Article.find(params[:id])
+	end
 
+	def update
+  		@article = Article.find(params[:id])
+  		@article.update(article_params)
 
+  		flash.notice = "High five! Article '#{@article.title}'has been updated!"
+  			
+  		redirect_to article_path(@article)
+	end
 end
